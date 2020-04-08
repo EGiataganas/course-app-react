0b27da0be0a9db4d64537f983be60885eed5091d
Evangelos Giataganas <e.giataganas@gmail.com>
2020-04-08 20:45:02 +0100

    Render items in table using Thumbnail

---
+++ app/javascript/components/Home/Table/Item.js
@@ -0,0 +1,31 @@
+import React, {Component} from "react"
+import Thumbnail from './Thumbnail'
+
+const Item = (props) => {
+  return(
+    <div className="row">
+      <div className="col-md-10 offset-md-1">
+        <div className="text-center">
+          <div className="card px-5">
+            <div className="row">
+              <div className="col-md-4">
+                <Thumbnail/>
+              </div>
+              <div className="col-md-8">
+                <div className="pt-4 mt-4">
+                  <h4>{props.title}</h4>
+                  <p>{props.description}</p>
+                  <div className="cta-wrapper">
+                    <a className="cta-button">Watch this Video</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Item
