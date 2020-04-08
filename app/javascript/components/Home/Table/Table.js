7007a17b19a36cd41a5f6e5c3d7e0cd924d75e58
Evangelos Giataganas <e.giataganas@gmail.com>
2020-04-08 22:54:55 +0100

    Create ActiveItem and Video and render it at Table js

--- app/javascript/components/Home/Table/Table.js
+++ app/javascript/components/Home/Table/Table.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react"
 import Item from './Item'
+import ActiveItem from './ActiveItem'
 
 class Table extends Component {
   constructor(props) {
@@ -7,7 +8,11 @@ class Table extends Component {
   }
   render(){
     const items = this.props.course_modules.map((data) => {
-      return <Item key={data.id} title={data.title} description={data.description}/>
+      return(
+        data.active ?
+        <ActiveItem handleVideoChange={handleVideoChange} key={data.id} title={data.title} description={data.description}/> :
+        <Item handleVideoChange={handleVideoChange} key={data.id} title={data.title} description={data.description}/>
+      )
     })
 
     return(
